52df9c6b1de9c86d55c211adba79605dec14e82f
Ericsii <30063838+Ericsii@users.noreply.github.com>
2022-02-26 23:02:51 +0800

    feat: Add whole project

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# rmoss_mindvision_driver
+# rmoss_entity_cam模块
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+![](rmoss_bg.png)
+
+## 简介
+
+`rmoss_entity_cam`提供了大恒和MindVision USB3.0工业相机ROS节点相关功能。
+
+文件说明：
+
+* `daheng_cam.hpp.hpp/cpp` : 大恒相机设备实现。
+* `mindvision_cam.hpp.hpp/cpp`：MindVision相机设备实现。
+* `mindvision_cam_node.hpp/cpp` ，`daheng_cam_node.hpp/cpp` :  ROS顶层模块（基于`DaHengCam`,`MindVisionCam`和`CamServer`），实现大恒相机节点和MindVision相机节点。
+
+## 依赖
+
+* `rmoss_interfaces`
+* `rmoss_cam`
+* `OpenCV 4.x`
+
+## 使用方式
+
+> 参照`rmoss_core/rmoss_cam`工具包中定义
+
+### 大恒相机
+
+launch方式运行：
+
+```bash
+ros2 launch rmoss_entity_cam daheng_cam.launch.py
+```
+
+* 参数文件(`config/cam_params.yaml`)
+* 相关相机配置(`config/*`)
+
+### MindVision相机
+
+launch方式运行：
+
+```bash
+ros2 launch rmoss_entity_cam mindvision_cam.launch.py
+```
+
+* 参数文件(`config/cam_params.yaml`)
+* 相关相机配置(`config/*`)
---
+++ src/mindvision_cam_node.cpp
@@ -0,0 +1,75 @@
+// Copyright 2022 robomaster-oss.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "rmoss_mindvision_driver/mindvision_cam_node.hpp"
+
+#include <memory>
+#include <string>
+
+namespace rmoss_entity_cam
+{
+    MindVisionCamNode::MindVisionCamNode(
+        const rclcpp::NodeOptions &options)
+    {
+        node_ = std::make_shared<rclcpp::Node>("mv_cam", options);
+
+        node_->declare_parameter("sn", "");
+        node_->declare_parameter("config_path", "");
+
+        std::string cam_sn = node_->get_parameter("sn").as_string();
+        std::string config_path = node_->get_parameter("config_path").as_string();
+
+        RCLCPP_INFO(
+            node_->get_logger(),
+            "Try to open camera at sn:%s", cam_sn.c_str());
+        RCLCPP_INFO(
+            node_->get_logger(),
+            "Camera config file path:%s", config_path.c_str());
+
+        cam_dev_ = std::make_shared<MindVisionCam>(cam_sn, node_, config_path);
+
+        if (config_path == "")
+        {
+            if (!cam_dev_->open())
+            {
+                RCLCPP_WARN(
+                    node_->get_logger(),
+                    "Try to open camera failed!");
+                return;
+            }
+
+            RCLCPP_INFO(
+                node_->get_logger(),
+                "Writing camera config to file.");
+            if (!cam_dev_->save_config(cam_sn + ".config"))
+            {
+                RCLCPP_WARN(
+                    node_->get_logger(),
+                    "Write failed!");
+                return;
+            }
+            cam_dev_->close();
+        }
+
+        // create server
+        cam_server_ = std::make_shared<rmoss_cam::CamServer>(this->node_, this->cam_dev_);
+    }
+} // namespace rm_cam
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+// Register the component with class_loader.
+// This acts as a sort of entry point, allowing the component to be discoverable when its library
+// is being loaded into a running process.
+RCLCPP_COMPONENTS_REGISTER_NODE(rmoss_entity_cam::MindVisionCamNode)
